e188a73f4c09a0d3c684d5604bdcfcf15e8b3ec3
Gulsher Ahmed <gulsherahmed@Gulshers-MacBook-Pro.local>
2023-04-23 14:21:20 +0530

    home screen work

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { ChevronDownIcon , UserIcon , SearchIcon , AdjustmentsIcon  } from "react-native-heroicons/outline";
 
 export default function HomeScreen() {
+    const navigation = useNavigation();
+
+    useLayoutEffect(()=>{
+        navigation.setOptions({
+            headerShown:false
+        })
+    },[])
+
   return (
-    <View>
-      <Text className='text-red-500 text-center'>HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text className='text-red-500'>
+        {/* header */}
+        <View className='flex-row pb-2 items-center mx-4 space-x-2'>
+            <Image source={{
+                uri:`https://links.papareact.com/wru`
+            }} className='h-7 w-7 bg-gray-300 p-4 rounded-full' />
+            <View>
+            <Text className='font-bold text-gray-400 text-xs' >Deliver Now</Text>
+            <Text className='font-bold text-xl'>Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" /> 
+            </Text>
+        </View>
+        <UserIcon size={35} color="#00CCBB" />
+        </View>
+        
+      </Text>
+    </SafeAreaView>
   )
 }

b82cfd60a23b701702ff94d5b6f9c84a6ca17d67
Gulsher Ahmed <gulsherahmed@Gulshers-MacBook-Pro.local>
2023-05-07 01:07:36 +0530

    Basket Screen added

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React, { useMemo, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { useDispatch, useSelector } from 'react-redux';
 import { selectRestaurant } from '../features/restaurantSlice';
 import { selectBasketItem } from '../features/baksetSlice';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { XCircleIcon } from 'react-native-heroicons/outline';
 
 export default function BasketScreen() {
     const navigation = useNavigation();
@@ -27,6 +28,9 @@ export default function BasketScreen() {
                 <Text className='text-lg font-bold text-center'>Basket</Text>
                 <Text className='font-bold text-center text-gray-400'>{restaurant.title}</Text>
             </View>
+            <TouchableOpacity onPress={navigation.goBack} className='rounded-full bg-gray-100 absolute right-5' >
+                <XCircleIcon color='#00CCBB' height={50} width={50} />
+            </TouchableOpacity>
         </View>
       </View>
     </SafeAreaView>
